6013bc1f43c2147df87fb875458267f9076f6787
Gucci Fan <gucc@umich.edu>
2022-12-28 21:17:20 -0500

    commit tree 731077518664053be024250e3c209d6ee573f17f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize()
+        @knowledge=[]
+    end
+
+    def learn(stuff)
+        self.knowledge << stuff
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
